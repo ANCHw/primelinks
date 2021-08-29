b8d3a80051f2a2227f29d97198ffa8386b3ad90a
Margo_D <mard.mtx@gmail.com>
2021-08-29 15:31:33 +0300

    main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,50 @@
 $(document).ready(function () {
+  //плавный скролл
   $('a[href*="#"]').click(function() {
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
     return false;
   });
-  
+
+  //бургер
   $('.burger').click( function () {
     $('.sidemenu').toggleClass('active');
     $('body').toggleClass('fixed');
   });
-  
 
-  
+  //главный слайдер
+  $('.top__main-slider').owlCarousel({
+    loop:false,
+    nav:true,
+    items:1
+  });
 
+  //слайдер отзывов
+  $('.reviews__slider').owlCarousel({
+    loop:false,
+    nav:true,
+    items:3,
+    margin: 20,
+  });
 
+  //слайдер блога
+  $('.blog__slider').owlCarousel({
+    loop:false,
+    nav:true,
+    items:3,
+    margin: 20,
+  });
+
+  //запуск видео
+  $('.btn-play').click( function () {
+    let video = $(this).siblings('video').get(0);
+    if ( video.paused ) {
+      video.play();
+      $(".btn-play").addClass('active');
+    } else {
+      video.pause();
+      $(".btn-play").removeClass('active');
+    }
+  });
 });
